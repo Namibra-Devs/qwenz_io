7ea951db832c81683fc16104c0b26b5f4f3e1fc4
kindo <kindojnr58@gmail.com>
2025-08-21 20:01:54 +0000

    vite config updated

--- qwenz/vite.config.js
+++ qwenz/vite.config.js
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+  optimizeDeps: {
+    include: [
+      "framer-motion",
+      "react-icons",
+     
+    ],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    outDir: 'dist',
+  },
+});
